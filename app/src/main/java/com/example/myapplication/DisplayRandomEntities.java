a85e17d40d9cbe6b8e266fcd8f2afcda20a268fb
OttaviaFeletig <ottafel@hotmail.it>
2019-08-28 16:34:07 +0200

    display response posts

--- app/src/main/java/com/example/myapplication/DisplayRandomEntities.java
+++ app/src/main/java/com/example/myapplication/DisplayRandomEntities.java
@@ -3,34 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-
-import com.android.volley.Cache;
-import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HttpResponse;
-import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DisplayRandomEntities extends AppCompatActivity {
 
@@ -43,20 +25,20 @@ public class DisplayRandomEntities extends AppCompatActivity {
 
 
     private void getData(){
-        final TextView textView = (TextView) findViewById(R.id.textview_entities);
+        final TextView textView = findViewById(R.id.textview_entities);
         // Request a string response from the provided URL.
         String url = "https://jsonplaceholder.typicode.com/posts";
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response);
-                    }
+                    public void onResponse(JSONArray response) {
+
+                        displayPosts(response, textView);
+                                            }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -65,11 +47,19 @@ public class DisplayRandomEntities extends AppCompatActivity {
         });
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonArrayRequest);
     }
 
+    public void displayPosts(JSONArray posts, TextView textView){
+        try{
+            for(int i=0;i<posts.length();i++){
+                JSONObject post = posts.getJSONObject(i);
+                textView.append(post.getString("body"));
+                textView.append("\n\n");
 
-
-
-
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
 }
